dacacd65b31816009c87f3642727b1165d71ef1f
Yash Thorat <163547076+yashraj1080@users.noreply.github.com>
2026-05-20 00:27:07 +0530

    commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,8 @@
-export default function VehiclePage() {
+export default function TeamPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black text-white">
       <h1 className="text-6xl font-black uppercase">
-        Vehicle Page
+        Team Page
       </h1>
     </main>
   );
---
+++ app/gallery/page.tsx
@@ -0,0 +1,9 @@
+export default function GalleryPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-6xl font-black uppercase">
+        Gallery Page
+      </h1>
+    </main>
+  );
+}
